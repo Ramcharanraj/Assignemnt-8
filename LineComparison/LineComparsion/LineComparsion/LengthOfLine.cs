461aaa9a7e2d726867b1b817d200b890c9ab4738
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-28 22:13:25 +0530

    Add: Equality of Two Lines

--- LineComparison/LineComparsion/LineComparsion/LengthOfLine.cs
+++ LineComparison/LineComparsion/LineComparsion/LengthOfLine.cs
@@ -5,20 +5,9 @@ using System.Text;
 namespace LineComaprison
 {
 
-    class LengthOfLine
+    public class LengthOfLine
     {
-        private double X1;
-        private double X2;
-        private double Y1;
-        private double Y2;
-        public LengthOfLine(double X1, double X2, double Y1, double Y2)
-        {
-            this.X1 = X1;
-            this.X2 = X2;
-            this.Y1 = Y1;
-            this.Y2 = Y2;
-        }
-        public double Length()
+        public double Length(double X1, double X2, double Y1, double Y2)
         {
             double lengthofline = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
             Console.WriteLine(lengthofline);
--- LineComparison/LineComparsion/LineComparsion/Program.cs
+++ LineComparison/LineComparsion/LineComparsion/Program.cs
@@ -6,9 +6,19 @@ namespace LineComparsion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LineComparision");
-            LengthOfLine length = new LengthOfLine(4, 6, 8, 10);
-            Console.WriteLine(length);
+            //length of lines
+            LengthOfLine line1 = new LengthOfLine();
+            LengthOfLine line2 = new LengthOfLine();
+            double a = line1.Length(2, 4, 8, 12);
+            double b = line2.Length(2, 4, 8, 12);
+            if (a == b)
+            {
+                Console.WriteLine("Lines Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines Are not Equals");
+            }
         }
     }
 }
