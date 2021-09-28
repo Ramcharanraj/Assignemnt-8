ed3a213ad562a05e09ef281531aba7aaa766bb43
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-28 22:19:15 +0530

    Add: Comparing Lines

--- LineComparison/LineComparsion/LineComparsion/LengthOfLine.cs
+++ LineComparison/LineComparsion/LineComparsion/LengthOfLine.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace LineComaprison
+namespace LineComparsion
 {
-
     public class LengthOfLine
     {
         public double Length(double X1, double X2, double Y1, double Y2)
@@ -12,6 +11,7 @@ namespace LineComaprison
             double lengthofline = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
             Console.WriteLine(lengthofline);
             return lengthofline;
+
         }
     }
 }
