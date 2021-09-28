e1daba11c1cd04d3cda9d9ae7dbeecb8ec1a1bf2
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-28 22:25:10 +0530

    Add:Using Methods

--- LineComparison/LineComparsion/LineComparsion/LengthOfLine.cs
+++ LineComparison/LineComparsion/LineComparsion/LengthOfLine.cs
@@ -4,9 +4,13 @@ using System.Text;
 
 namespace LineComparsion
 {
-    public class LengthOfLine
+    abstract class Length
     {
-        public double Length(double X1, double X2, double Y1, double Y2)
+        public abstract double Line(double X1, double X2, double Y1, double Y2);
+    }
+    class LengthOfLine : Length
+    {
+        public override double Line(double X1, double X2, double Y1, double Y2)
         {
             double lengthofline = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
             Console.WriteLine(lengthofline);
--- LineComparison/LineComparsion/LineComparsion/Program.cs
+++ LineComparison/LineComparsion/LineComparsion/Program.cs
@@ -6,25 +6,22 @@ namespace LineComparsion
     {
         static void Main(string[] args)
         {
-            //length of lines
-            LengthOfLine line1 = new LengthOfLine();
-            LengthOfLine line2 = new LengthOfLine();
-            LengthOfLine line3 = new LengthOfLine();
-            double a = line1.Length(2, 8, 8, 12);
-            double b = line2.Length(2, 7, 8, 12);
-            double c = (a.CompareTo(b));
-            if (c == 0)
-            {
-                Console.WriteLine("Lines Are Equal");
-            }
-            else if (c == 1)
-            {
-                Console.WriteLine("Line1 is greater than Line2");
-            }
+            Console.WriteLine("Welcome to LineComparision");
+            LengthOfLine length = new LengthOfLine();
+            double Line1 = length.Line(1, 2, 3, 2);
+            double Line2 = length.Line(1, 2, 4, 6);
+
+            //Lines Equals
+            if (Line1 == Line2)
+                Console.WriteLine("lines are equal");
             else
-            {
-                Console.WriteLine("line2 is grater than line1");
-            }
+                Console.WriteLine("lines are not equal");
+
+            //compare lines
+            if (Line1.CompareTo(Line2) == 0)
+                Console.WriteLine("Both Line are  equal");
+            else
+                Console.WriteLine("They are not equal");
         }
     }
 }
